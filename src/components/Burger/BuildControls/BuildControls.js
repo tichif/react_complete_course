265832f2536b1156dd8da91a1200d36333b7f6f8
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-01-26 22:05:13 -0500

    Add ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,11 +10,15 @@ const controls = [
   { label: 'Meat', type: 'meat' },
 ];
 
-const BuildControls = () => {
+const BuildControls = ({ ingredientAdded }) => {
   return (
     <div className={classes.BuildControls}>
       {controls.map((ctrl) => (
-        <BuildControl key={ctrl.label} label={ctrl.label} />
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => ingredientAdded(ctrl.type)}
+        />
       ))}
     </div>
   );
