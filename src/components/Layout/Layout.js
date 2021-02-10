b2c00e2922c326c90d064bd7ce17b960f1df9a4b
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-02-10 14:51:13 -0500

    Create a Responsible SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import classes from './Layout.css';
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = ({ children }) => {
   return (
     <Auxiliary>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{children}</main>
     </Auxiliary>
   );
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-import classes from './Toolbar.css';
+import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
-const Toolbar = () => {
+const SideDrawer = () => {
   return (
-    <header className={classes.Toolbar}>
-      <div>MENU</div>
+    <div className={classes.SideDrawer}>
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <nav className={classes.DesktopOnly}>
+      <nav>
         <NavigationItems />
       </nav>
-    </header>
+    </div>
   );
 };
 
-export default Toolbar;
+export default SideDrawer;
