5376a6bc11aba2f592a132b415cc878d736b03d2
Charles-Hebert Dalzon <charlby5@gmail.com>
2021-02-10 05:36:23 -0500

    Create the Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import classes from './Layout.css';
 import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = ({ children }) => {
   return (
     <Auxiliary>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>{children}</main>
     </Auxiliary>
   );
